7cbe16c6ddc539e96b9fa49a538bf6cc83053b4c
Jonathan Holloway <jonathan.holloway@gmail.com>
2014-10-24 09:38:05 +0100

    Update getattributevalue.sh

--- getattributevalue.sh
+++ getattributevalue.sh
@@ -1,1 +1,1 @@
-java -jar target/jmxclient.jar get nav90el6perf:8090 navplus n@vp1us com.mchange.v2.c3p0:type=PooledDataSource.* numBusyConnections true 1
+java -jar target/jmxclient.jar get hostname:8090 user pass com.mchange.v2.c3p0:type=PooledDataSource.* numBusyConnections true 1
